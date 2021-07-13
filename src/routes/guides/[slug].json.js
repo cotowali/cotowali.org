15b490b5a6800a7eec6a6797396671e986e9a0e5
zakuro <z@kuro.red>
2021-07-13 13:05:56 +0900

    src/pages -> contents

--- src/routes/guides/[slug].json.js
+++ src/routes/guides/[slug].json.js
@@ -2,7 +2,7 @@ import { process } from '$lib/markdown'
 
 export async function get({ params }) {
   const { slug } = params
-  const { metadata, content } = await process(`src/pages/guides/${slug}.md`)
+  const { metadata, content } = await process(`contents/guides/${slug}.md`)
   const body = JSON.stringify({ metadata, content })
   return { body }
 }
