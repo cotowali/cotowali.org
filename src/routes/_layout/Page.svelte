a2ecb7f807a82b9a451d9009ae9990f90efc8ac4
zakuro <z@kuro.red>
2021-07-14 14:31:57 +0900

    update nav

--- src/routes/_layout/Page.svelte
+++ src/routes/_layout/Page.svelte
@@ -1,24 +1,35 @@
 <div class="page">
   <header class="header">
-    <span>Cotowali</span>
+    <div class="flex items-center">
+      <span>Cotowali</span>
+    </div>
     <nav>
+      <ul class="flex h-full">
+        <li><a class="h-full flex items-center">Guide</a></li>
+        <li><a class="h-full flex items-center">GitHub</a></li>
+      </ul>
     </nav>
   </header>
 
   <div class="content">
     hello layout
     <slot></slot>
+    <span class="border inline-block align-bottom h-24">XXXX</span>
   </div>
 </div>
 
 <style>
   .page {
-    @apply min-h-full
+    @apply min-h-full;
   }
   .header {
-    @apply w-full flex shadow fixed h-12
+    @apply flex items-stretch fixed w-full h-12;
+    @apply shadow;
+  }
+  .header-item{
+    @apply h-12 inline-block align-middle leading-none;
   }
   .content {
-    @apply pt-12
+    @apply pt-12;
   }
 </style>
