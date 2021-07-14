d29f9e3eae11f0b49a55a3bd7659dc540e8cbe27
zakuro <z@kuro.red>
2021-07-15 04:04:06 +0900

    Add image to hero

--- src/routes/_top/Hero.svelte
+++ src/routes/_top/Hero.svelte
@@ -1,6 +1,11 @@
+<script>
+  import logo from '@cotowali/design/assets/cotowali.svg'
+</script>
+
 <div>
   <div class="flex flex-col">
     <h1>Cotowali</h1>
     <p>A statically typed script language that transpile into POSIX sh</p>
   </div>
+  <img src={logo} class="h-60">
 </div>
