d0e3bee72f4c28e6abe48e8bf95f8f86a6778a41
zakuro <z@kuro.red>
2021-07-15 13:50:48 +0900

    update hero

--- src/routes/_top/Hero.svelte
+++ src/routes/_top/Hero.svelte
@@ -4,8 +4,10 @@
 
 <div class="hero">
   <div class="flex flex-col">
-    <h1 class="text-main">Cotowali</h1>
-    <p>A statically typed script language that transpile into POSIX sh</p>
+    <h1 class="text-main text-4xl">Cotowali</h1>
+    <div class="mt-1 px-[3px]">
+      <p>A statically typed script language that transpile into POSIX sh</p>
+    </div>
   </div>
   <img src={logo} class="h-60">
 </div>
