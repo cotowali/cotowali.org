dd54be26fa52b1329b19f430038de2dff600045e
zakuro <z@kuro.red>
2021-07-15 13:38:03 +0900

    layout hero

--- src/routes/_top/Hero.svelte
+++ src/routes/_top/Hero.svelte
@@ -2,10 +2,16 @@
   import logo from '@cotowali/design/assets/cotowali.svg'
 </script>
 
-<div>
+<div class="hero">
   <div class="flex flex-col">
     <h1 class="text-main">Cotowali</h1>
     <p>A statically typed script language that transpile into POSIX sh</p>
   </div>
   <img src={logo} class="h-60">
 </div>
+
+<style>
+.hero {
+  @apply flex justify-center items-center;
+}
+</style>
