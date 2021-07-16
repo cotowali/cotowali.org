a3d57de324ed05d284525898269af73abb7f0cf8
zakuro <z@kuro.red>
2021-07-16 20:56:02 +0900

    colors.main -> colors.brand.red

--- src/routes/_top/Hero.svelte
+++ src/routes/_top/Hero.svelte
@@ -4,7 +4,7 @@
 
 <div class="hero">
   <div class="flex flex-col">
-    <h1 class="text-main text-4xl">Cotowali</h1>
+    <h1 class="text-brand-red text-4xl">Cotowali</h1>
     <div class="mt-1 px-[3px]">
       <p>A statically typed script language that transpile into POSIX sh</p>
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,9 @@ const config = {
       DEFAULT: '0 2px 3px 2px rgba(0, 0, 0, 0.5)',
     },
     colors: {
-      main: colors.brand.red.hex(),
+      brand: {
+        red: colors.brand.red.hex(),
+      },
       dark: colors.dark.hex(),
       'dark-darken-1': colors.dark.set('hsl.l', 0.018).hex(),
       white: colors.white.hex(),
