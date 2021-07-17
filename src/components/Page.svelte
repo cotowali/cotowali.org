e80c5d7514e9b054612c5338861fd5ae04a6a5f6
zakuro <z@kuro.red>
2021-07-17 15:10:42 +0900

    add copyright

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -19,13 +19,18 @@
     </nav>
   </header>
 
-  <div class="content">
+  <div class="content mh-full">
     <slot></slot>
   </div>
+
+  <footer class="footer">
+    Copyright &copy; 2021 The Cotowali Authors. All rights reserved.
+  </footer>
 </div>
 
 <style>
   .page {
+    @apply flex flex-col justify-between;
     @apply min-h-full;
     @apply bg-dark;
     @apply text-white;
@@ -35,6 +40,10 @@
     @apply fixed w-full h-12;
     @apply px-4;
     @apply bg-brand-red shadow;
+  }
+  .footer {
+    @apply text-center;
+    @apply p-4;
   }
   .nav-item-anchor {
     @apply h-full flex items-center; /* align text middle */
