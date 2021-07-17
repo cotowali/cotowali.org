1bfb0bf5c1d677dc05b17623c6a9852a35b9404a
zakuro <z@kuro.red>
2021-07-17 13:02:34 +0900

    add Button

---
+++ src/components/Button.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let type = 'button'
+  export let href
+
+  $: classNames = 'button'
+  $: tag = href && 'a' || 'button'
+</script>
+
+{#if tag === 'a'}
+  <a class="{classNames}" {href} {...$$restProps}><slot /></a>
+{:else}
+  <button class="{classNames}" {type} {...$$restProps}><slot/></button>
+{/if}
+
+<style>
+  .button {
+    @apply shadow;
+  }
+</style>
