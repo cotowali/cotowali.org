3578025547bece8fcd3ba5c60efbaecb2f7feb8f
zakuro <z@kuro.red>
2021-07-19 03:28:41 +0900

    add padding

--- src/routes/docs/_Sidenav.svelte
+++ src/routes/docs/_Sidenav.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="pr-40 min-h-full shadow">
-  <nav class="fixed w-40">
+  <nav class="fixed w-40 p-6">
     <ul>
       {#each entries as entry}
         <li>{entry.metadata.title}</li>
