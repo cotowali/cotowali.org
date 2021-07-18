48789499512b6e2b61e5623a934dfd513966f410
zakuro <z@kuro.red>
2021-07-19 04:15:31 +0900

    sidenav link hover

--- src/routes/docs/_Sidenav.svelte
+++ src/routes/docs/_Sidenav.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <div class="pr-40 min-h-full shadow">
-  <nav class="fixed w-40 p-6">
+  <nav class="fixed w-40 pt-6">
     <ul>
       {#each entries as { metadata: { title, path: href } }}
-        <li><a {href}>{title}</a></li>
+        <li><a {href} class="hover:bg-dark-darken-1 px-6 block">{title}</a></li>
       {/each}
   </nav>
 </div>
