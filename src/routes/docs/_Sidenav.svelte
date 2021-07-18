d2380e90c5afcb6f327c61aad1f5c4f4339d88f0
zakuro <z@kuro.red>
2021-07-19 03:08:50 +0900

    fixed sidenav

--- src/routes/docs/_Sidenav.svelte
+++ src/routes/docs/_Sidenav.svelte
@@ -2,9 +2,11 @@
   export let entries
 </script>
 
-<nav>
-  <ul>
-    {#each entries as entry}
-      <li>{entry.metadata.title}</li>
-    {/each}
-</nav>
+<div class="pr-40">
+  <nav class="fixed w-40">
+    <ul>
+      {#each entries as entry}
+        <li>{entry.metadata.title}</li>
+      {/each}
+  </nav>
+</div>
