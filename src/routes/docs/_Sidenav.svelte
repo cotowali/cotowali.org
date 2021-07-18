4c7d76b3d551b90978c088cb1330ee3ef4b30689
zakuro <z@kuro.red>
2021-07-19 03:49:03 +0900

    sidenav item link

--- src/routes/docs/_Sidenav.svelte
+++ src/routes/docs/_Sidenav.svelte
@@ -5,8 +5,8 @@
 <div class="pr-40 min-h-full shadow">
   <nav class="fixed w-40 p-6">
     <ul>
-      {#each entries as entry}
-        <li>{entry.metadata.title}</li>
+      {#each entries as { metadata: { title, path: href } }}
+        <li><a {href}>{title}</a></li>
       {/each}
   </nav>
 </div>
