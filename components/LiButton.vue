e2b500152725cc6554cc48dcd9faa6d53c8a59fc
zakuro <z@kuro.red>
2021-07-20 02:51:35 +0900

    add LiButton

---
+++ components/LiButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <a v-if="type === 'a'" class="button" :href="href"><slot /></a>
+  <button v-else class="button" :type="type">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'LiButton',
+  props: {
+    type: {
+      type: String,
+      default: 'button',
+    },
+    href: {
+      type: String,
+      default: null,
+    },
+  },
+})
+</script>
+
+<style>
+  .button {
+    @apply px-6 py-3;
+    @apply rounded shadow;
+  }
+</style>
