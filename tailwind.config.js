727ac825a4c8867e18cf9be5080523b264efa846
zakuro <z@kuro.red>
2021-07-20 02:17:51 +0900

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+const { colors } = require('@cotowali/design')
+
+module.exports = {
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+    boxShadow: {
+      DEFAULT: '0 3px 1px 0 rgba(0, 0, 0, 0.3), 0 0 4px 0 rgba(0, 0, 0, 0.5)',
+    },
+    borderRadius: {
+      DEFAULT: '0.125rem',
+    },
+    colors: {
+      brand: {
+        red: colors.brand.red.hex(),
+        'red-lighten-1': colors.brand.red.lightness(53).hex(),
+        'dark-red': colors.brand.darkRed.hex(),
+      },
+      dark: colors.dark.hex(),
+      'dark-darken-1': colors.dark.lightness(1.8).hex(),
+      white: colors.white.hex(),
+    },
+  },
+  plugins: [],
+}
