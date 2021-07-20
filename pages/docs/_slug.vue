8ef3797351546eba6b0191e68c9faaf218c9e05b
zakuro <z@kuro.red>
2021-07-20 12:49:55 +0900

    add pages/docs/_slug.vue

---
+++ pages/docs/_slug.vue
@@ -0,0 +1,25 @@
+<template>
+  <article class="p-12">
+    <h1 class="title">{{ entry.title }}</h1>
+    <nuxt-content :document="entry" />
+  </article>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ $content, params: { slug }}) {
+    const entry = await $content('docs', slug).fetch()
+    return { entry }
+  },
+})
+</script>
+
+<style>
+  .title {
+    @apply text-3xl text-brand-red font-black;
+    @apply pb-2;
+    @apply border-b-2;
+  }
+</style>
