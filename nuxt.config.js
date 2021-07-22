66c15b5f4a304748c626ec3654512238cc9ecf56
zakuro <z@kuro.red>
2021-07-22 23:59:21 +0900

    cleanup config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,6 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'cotowali.org',
     meta: [
@@ -16,49 +14,30 @@ export default {
     ],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  components: true,
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '@/plugins/vue-scrollactive',
     '@/plugins/docs',
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     '@nuxtjs/pwa',
     '@nuxt/content',
-    ['nuxt-i18n', {
-      vueI18nLoader: true,
-      detectBrowserLanguage: {
-        useCookie: true,
-        cookieKey: 'i18n_redirected',
-        onlyOnRoot: true,
-      },
-    }],
+    'nuxt-i18n',
   ],
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
       lang: 'en',
     },
   },
 
-  // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {
     markdown: {
       prism: {
@@ -67,16 +46,17 @@ export default {
     },
   },
 
-  // i18n
   i18n: {
+    vueI18nLoader: true,
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      onlyOnRoot: true,
+    },
     locales: [
       { code: 'en', iso: 'en-US' },
       { code: 'ja', iso: 'ja-JP' },
     ],
     defaultLocale: 'en',
-  },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
   },
 }
