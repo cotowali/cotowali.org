efa7d92abbe6b87188296ac757666c83580f836d
zakuro <z@kuro.red>
2021-07-22 15:50:33 +0900

    add LiIcon

---
+++ components/LiIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <span style="height: 24px; width: 24px;" class="fill-current">
+    <svg role="img" viewBox="0 0 24 24">
+      <path :d="icon" />
+    </svg>
+  </span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'LiIcon',
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
