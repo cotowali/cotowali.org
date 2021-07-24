a5dd484ba2644e1ccb9da7453f9064f60cd2267e
zakuro <z@kuro.red>
2021-07-24 15:55:52 +0900

    LiCollapsable(WIP)

---
+++ components/LiCollapsable.vue
@@ -0,0 +1,28 @@
+<template>
+  <transition name="collapsable" @before-enter="beforeEnter" @enter="enter" @beforeLeave="beforeLeave" @leave="leave">
+    <slot class="body" />
+  </transition>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+// TODO: WIP
+export default Vue.extend({
+  name: 'LiCollapsable',
+  methods: {
+    beforeEnter(_el: HTMLElement) {
+      // TODO
+    },
+    enter(_el: HTMLElement) {
+      // TODO
+    },
+    beforeLeave(_el: HTMLElement) {
+      // TODO
+    },
+    leave(_el: HTMLElement) {
+      // TODO
+    },
+  },
+})
+</script>
