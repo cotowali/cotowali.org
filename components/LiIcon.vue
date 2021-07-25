19dfad07fcbabf6f5aee5bd4075ba45075f5705a
zakuro <z@kuro.red>
2021-07-26 03:10:46 +0900

    add aria-label prop to LiIcon

--- components/LiIcon.vue
+++ components/LiIcon.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fill-current" :style="{ width: size, height: size }">
-    <svg role="img" viewBox="0 0 24 24" :width="size" :height="size">
+    <svg :aria-label="ariaLabel" role="img" viewBox="0 0 24 24" :width="size" :height="size">
       <path :d="icon" />
     </svg>
   </div>
@@ -19,6 +19,10 @@ export default Vue.extend({
     size: {
       type: String,
       default: '1.5rem',
+    },
+    ariaLabel: {
+      type: String,
+      required: true,
     },
   },
 })
