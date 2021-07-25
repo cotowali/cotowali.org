89f7f60204b40d4835373e91ea2bcd4ff8e6eabb
zakuro <z@kuro.red>
2021-07-26 00:14:09 +0900

    Revert "setup tailwind/typography"
    This reverts commit 6ff76c755f7b3f5e135a2b185c3a3b8129af7cee.

--- components/DocsPage.vue
+++ components/DocsPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <article class="p-12 pt-6 prose">
+  <article class="p-12 pt-6">
     <h1 class="title">{{ page.title }}</h1>
     <nuxt-content :document="page" />
   </article>
--- tailwind.config.js
+++ tailwind.config.js
@@ -51,7 +51,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [],
 }
