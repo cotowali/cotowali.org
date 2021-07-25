da94578e089742f07e9f347b286d68bcaf46f8e3
zakuro <z@kuro.red>
2021-07-25 15:12:09 +0900

    LiSidenav.vue

--- components/DocsSidenav.vue
+++ components/DocsSidenav.vue
@@ -1,63 +1,22 @@
 <template>
-  <div class="sidenav-wrapper" :class="{ closed }">
-    <nav class="sidenav" :class="{ closed }">
-      <ul>
-        <li v-for="page in pages" :key="page.slug">
-          <DocsSidenavItem class="px-6" :page="page" />
-        </li>
-      </ul>
-    </nav>
-    <LiButton icon dark-red class="menu-button" @click="closed = !closed">
-      <LiIcon :icon="mdiMenu" />
-    </LiButton>
-  </div>
+  <LiSidenav>
+    <ul>
+      <li v-for="page in pages" :key="page.slug">
+        <DocsSidenavItem class="px-6" :page="page" />
+      </li>
+    </ul>
+  </LiSidenav>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { Page } from '@/plugins/docs'
-import { mdiMenu } from '@mdi/js'
 
 export default Vue.extend({
   name: 'DocsSidenav',
-  data: () => ({
-    closed: false,
-    pages: [] as Page[],
-    mdiMenu,
-  }),
+  data: () => ({ pages: [] as Page[] }),
   async fetch() {
     this.pages = await this.$docs.fetch()
   },
 })
 </script>
-
-<style scoped>
-.sidenav-wrapper {
-  @apply pr-64 bg-dark-darken-1 min-h-full;
-  transition-property: padding-right;
-}
-
-.sidenav-wrapper.closed {
-  @apply pr-0;
-}
-
-.sidenav {
-  @apply fixed w-64 pt-6;
-  @apply overflow-y-scroll h-full;
-  transition-property: transform;
-  @apply translate-x-0;
-}
-
-.sidenav.closed {
-  @apply -translate-x-full;
-}
-
-.sidenav, .sidenav-wrapper {
-  @apply duration-300 ease-out;
-}
-
-.menu-button {
-  @apply fixed;
-  @apply left-[20px] bottom-[20px];
-}
-</style>
---
+++ components/LiSidenav.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="sidenav-wrapper" :class="{ closed }">
+    <nav class="sidenav" :class="{ closed }">
+      <slot />
+    </nav>
+    <LiButton icon dark-red class="menu-button" @click="closed = !closed">
+      <LiIcon :icon="mdiMenu" />
+    </LiButton>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mdiMenu } from '@mdi/js'
+
+export default Vue.extend({
+  name: 'LiSidenav',
+  data: () => ({
+    closed: false,
+    mdiMenu,
+  }),
+})
+</script>
+
+<style scoped>
+.sidenav-wrapper {
+  @apply pr-64 min-h-full;
+  transition-property: padding-right;
+}
+
+.sidenav-wrapper.closed {
+  @apply pr-0;
+}
+
+.sidenav {
+  @apply fixed w-64 pt-6;
+  @apply overflow-y-scroll h-full;
+  transition-property: transform;
+  @apply translate-x-0;
+}
+
+.sidenav.closed {
+  @apply -translate-x-full;
+}
+
+.sidenav, .sidenav-wrapper {
+  @apply duration-300 ease-out;
+}
+
+.menu-button {
+  @apply fixed;
+  @apply left-[20px] bottom-[20px];
+}
+</style>
