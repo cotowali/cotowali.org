c5c3eb4f51acc7243d43820b0fddbdb81bb4a7a6
zakuro <z@kuro.red>
2021-07-26 00:38:26 +0900

    docs h2-h6 style

--- components/DocsPage.vue
+++ components/DocsPage.vue
@@ -20,6 +20,28 @@ export default Vue.extend({
 })
 </script>
 
+<style>
+.nuxt-content h2 {
+  @apply text-2xl font-bold font-title;
+  @apply mt-12 mb-6;
+}
+.nuxt-content h3 {
+  @apply text-xl font-bold font-title;
+  @apply mt-6 mb-2;
+}
+.nuxt-content h4 {
+  @apply text-lg font-bold;
+  @apply mt-6;
+}
+.nuxt-content h5 {
+  @apply font-bold;
+  @apply mt-4;
+}
+.nuxt-content h6 {
+  @apply font-bold;
+  @apply mt-4;
+}
+</style>
 
 <style scoped>
 .title {
