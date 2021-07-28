4fa8f2f31e6afc81d28366244279bfa6e1367462
zakuro <z@kuro.red>
2021-07-28 15:19:35 +0900

    update textlint rules

--- .textlintrc.ja.js
+++ .textlintrc.ja.js
@@ -4,5 +4,10 @@ const base = require('./.textlintrc.base.js')
 module.exports = merge(base, {
   rules: {
     'preset-ja-technical-writing': true,
+    'ja-no-redundant-expression': {
+      dictOptions: {
+        dict2: { disabled: true },
+      },
+    },
   },
 })
