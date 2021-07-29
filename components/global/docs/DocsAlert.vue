c3a419e36349bb339dc810ae5d9b02ab702fc9ad
zakuro <z@kuro.red>
2021-07-30 01:05:37 +0900

    add DocsAlert

---
+++ components/global/docs/DocsAlert.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="alert">
+    <LiIcon class="alert-icon" :icon="icon" :aria-label="iconLabel" />
+    <div class="alert-content">
+      <slot />
+    </diV>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mdiAlert } from '@mdi/js'
+
+export default Vue.extend({
+  name: 'DocsAlert',
+  props: {
+    icon: {
+      type: String,
+      default: () => mdiAlert,
+    },
+    iconLabel: {
+      type: String,
+      default: () => 'alert',
+    },
+  },
+})
+</script>
+
+<style scoped>
+.alert {
+  @apply flex items-start;
+  @apply p-4 my-4;
+  @apply bg-red-300 text-red-600;
+  @apply border-2 border-current;
+  @apply rounded;
+}
+
+.alert-icon {
+  @apply flex-shrink-0;
+}
+
+.alert-content {
+  @apply flex-grow;
+  @apply ml-2;
+}
+</style>
