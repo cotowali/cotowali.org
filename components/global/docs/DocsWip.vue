d17ad903af61170ddf83cbc8c940fe14b763d5e0
zakuro <z@kuro.red>
2021-07-31 01:07:32 +0900

    add DocsWip

---
+++ components/global/docs/DocsWip.vue
@@ -0,0 +1,17 @@
+<i18n>
+{
+  "en": {
+    "wip": "Work In Progress"
+  },
+  "ja": {
+    "wip": "執筆中"
+  }
+}
+</i18n>
+
+<template>
+  <DocsAlert>
+    <p>{{ $t('wip') }}</p>
+    <slot />
+  </DocsAlert>
+</template>
