92c4227ca42fccf6844a6ba00b320a13d92d263a
zakuro <z@kuro.red>
2021-08-09 21:26:14 +0900

    update textlintrc

--- .textlintrc.ja.js
+++ .textlintrc.ja.js
@@ -4,7 +4,7 @@ const base = require('./.textlintrc.base.js')
 module.exports = merge(base, {
   rules: {
     'preset-ja-technical-writing': true,
-    'ja-no-redundant-expression': {
+    'ja-technical-writing/ja-no-redundant-expression': {
       dictOptions: {
         dict2: { disabled: true },
       },
