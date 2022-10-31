777723f758b5790020a12b1109d258371b520075
zakuro <z@kuro.red>
2022-11-01 08:51:57 +0900

    add LiFooter

---
+++ components/LiFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="footer">
+    <p>Copyright &copy; zakuro &lt;z@kuro.red&gt;</p>
+    <p>Design by zakuro &amp; Yuki Amane (Pianoforte)</p>
+  </footer>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'LiFooter',
+}
+</script>
+
+<style scoped>
+.footer {
+  @apply text-center text-2xs opacity-75;
+  @apply p-4;
+}
+</style>
