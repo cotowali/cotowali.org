40c9b06d2d6832dd8492bf6d387ab1764d687c0f
zakuro <z@kuro.red>
2022-11-01 09:01:02 +0900

    add LiIcon

---
+++ components/LiIcon.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="fill-current" :style="{ width: size, height: size }">
+    <svg
+      v-bind="$attrs"
+      role="img"
+      viewBox="0 0 24 24"
+      :width="size"
+      :height="size"
+    >
+      <path :d="icon" />
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'LiIcon',
+  inheritAttrs: false,
+}
+</script>
+
+<script setup lang="ts">
+defineProps({
+ icon: {
+   type: String,
+   required: true,
+ },
+ size: {
+   type: String,
+   default: '1.5rem',
+ },
+})
+</script>
