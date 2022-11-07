911fcbc8654f4ecb90f6a672fbf00101690ee07a
zakuro <z@kuro.red>
2022-11-07 10:04:25 +0900

    migrate tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,14 +23,6 @@ const fontMono = [
 ]
 
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './components/**/*.{vue,js}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
-  ],
   theme: {
     extend: {
       minHeight: defaultTheme.spacing,
