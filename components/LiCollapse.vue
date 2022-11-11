c9a944886e00cce749da21eeb3674deb8c69f148
zakuro <z@kuro.red>
2022-11-12 07:44:50 +0900

    Fix LiCollapse

--- components/LiCollapse.vue
+++ components/LiCollapse.vue
@@ -4,7 +4,6 @@
     @enter="enter"
     @after-enter="afterEnter"
     @leave="leave"
-    @after-leave="afterLeave"
   >
     <div v-show="expanded">
       <slot />
@@ -13,53 +12,51 @@
 </template>
 
 <script lang="ts">
-// https://markus.oberlehner.net/blog/transition-to-height-auto-with-vue/
 export default {
   name: 'LiCollapse',
-  props: {
-    expanded: {
-      type: Boolean,
-    },
+}
+</script>
+
+<script setup lang="ts">
+defineProps({
+  expanded: {
+    type: Boolean,
   },
-  methods: {
-    enter(el: HTMLElement) {
-      const { width } = getComputedStyle(el)
-      el.style.setProperty('width', width)
-      el.style.setProperty('position', 'absolute')
-      el.style.setProperty('visibility', 'hidden')
-      el.style.setProperty('height', 'auto')
+})
 
-      const { height } = getComputedStyle(el)
-      el.style.setProperty('width', null)
-      el.style.setProperty('position', null)
-      el.style.setProperty('visibility', null)
-      el.style.setProperty('height', '0')
+// https://markus.oberlehner.net/blog/transition-to-height-auto-with-vue/
 
-      // Force repaint to make sure the
-      // animation is triggered correctly
-      getComputedStyle(el).height // eslint-disable-line no-unused-expressions
-      requestAnimationFrame(() => {
-        el.style.height = height
-      })
-    },
-    afterEnter(el: HTMLElement) {
-      el.style.height = 'auto'
-    },
-    leave(el: HTMLElement) {
-      const { height } = getComputedStyle(el)
-      el.style.height = height
+const enter = (el: HTMLElement) => {
+  const { width } = getComputedStyle(el)
+  el.style.width = width
+  el.style.position = 'absolute'
+  el.style.visibility = 'hidden'
+  el.style.height = 'auto'
 
-      // Force repaint to make sure the
-      // animation is triggered correctly.
-      getComputedStyle(el).height // eslint-disable-line no-unused-expressions
-      requestAnimationFrame(() => {
-        el.style.height = '0'
-      })
-    },
-    afterLeave(_el: HTMLElement) {
-      // eslint-disable-line
-    },
-  },
+  const { height } = getComputedStyle(el)
+
+  el.style.width = null
+  el.style.position = null
+  el.style.visibility = null
+  el.style.height = 0
+
+  getComputedStyle(el).height // eslint-disable-line no-unused-expressions
+  requestAnimationFrame(() => {
+    el.style.height = height
+  })
+}
+
+const afterEnter = (el: HTMLElement) => {
+  el.style.height = 'auto'
+}
+
+const leave = (el: HTMLElement) => {
+  el.style.height = getComputedStyle(el).height
+
+  getComputedStyle(el).height // eslint-disable-line no-unused-expressions
+  requestAnimationFrame(() => {
+    el.style.height = '0'
+  })
 }
 </script>
 
