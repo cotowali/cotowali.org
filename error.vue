3c3d82feb697128b45cef72789d18e49a0f82f23
zakuro <z@kuro.red>
2022-12-01 08:38:24 +0900

    Add error page

---
+++ error.vue
@@ -0,0 +1,45 @@
+<template>
+  <LiMeta />
+  <LiPage no-footer>
+    <div class="error-page">
+      <div class="flex flex-col items-center">
+        <h1 class="text-6xl font-bold text-brand-red">
+          {{ error.statusCode }}
+        </h1>
+        <div v-if="error.statusCode === '404'">
+          Page not found
+        </div>
+        <div v-else>
+          An error occurred
+        </div>
+      </div>
+    </div>
+
+    <div class="absolute w-full flex justify-center top-2/3">
+      <nuxt-link
+        to="/"
+        class="font-bold text-brand-red hover:underline"
+      >
+        Go back to Home
+      </nuxt-link>
+    </div>
+  </LiPage>
+</template>
+
+<script lang="ts">
+export default {
+  layout: 'raw',
+}
+</script>
+
+<script setup lang="ts">
+const error = useError()
+</script>
+
+<style scoped>
+.error-page {
+  @apply fixed h-screen w-screen -top-header-height;
+  @apply flex flex-col items-center justify-center;
+  @apply gap-y-6;
+}
+</style>
