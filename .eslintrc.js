d583383af1fe39ac90cf89c2d3a80d4428145774
zakuro <z@kuro.red>
2023-02-16 09:42:48 +0900

    use `@cotowali/eslint-config`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,15 @@
 module.exports = {
-  env: {
-    es2021: true,
-    browser: true,
-    node: true,
-  },
   extends: [
-    'standard',
-    'plugin:@typescript-eslint/recommended',
+    '@cotowali/eslint-config/typescript',
     'plugin:vue/vue3-recommended',
     'plugin:nuxt/recommended',
-  ],
-  overrides: [
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
   rules: {
     'vue/multi-word-component-names': 'off',
-    'no-undef': 'off', // for auto-import
-    'comma-dangle': ['error', 'always-multiline'],
-    'space-before-function-paren': ['error', {
-      'anonymous': 'never',
-      'named': 'never',
-      'asyncArrow': 'always',
-    }],
-    'no-mixed-operators': 'off',
-    'template-curly-spacing': ['error', 'always'],
-    '@typescript-eslint/no-unused-vars': ['error', {
-      'varsIgnorePattern': '^_',
-      'argsIgnorePattern': '^_',
-    }],
-    'no-console': 'warn',
   },
 }
