b8178cc0319e9e1849b54b1774e8835f56aa6e23
zakuro <z@kuro.red>
2023-03-04 08:56:45 +0900

    add sponsors.ts

--- components/LiSponsors.vue
+++ components/LiSponsors.vue
@@ -75,35 +75,10 @@
 </template>
 
 <script setup lang="ts">
-const sponsors = [{
-  name: 'CubeSoft, Inc.',
-  image: 'https://raw.githubusercontent.com/cube-soft/Cube.Core/master/Icon.svg',
-  url: 'https://www.cube-soft.jp/',
-}]
-
-const personalSilverSponsors = [{
-  name: 'takesako',
-  image: 'https://github.com/takesako.png',
-  url: 'https://github.com/takesako',
-}, {
-  name: 'orisano',
-  image: 'https://github.com/orisano.png',
-  url: 'https://github.com/orisano',
-}]
-
-const personalBronzeSponsors = [{
-  name: 'Krouton',
-  image: 'https://github.com/Krout0n.png',
-  url: 'https://github.com/Krout0n',
-}, {
-  name: 'Kazuki Chigita',
-  image: 'https://pbs.twimg.com/profile_images/1480844416462225409/aiBGMxvt.jpg',
-  url: 'https://kazuki.chigita.net/profile',
-}]
-
-const personalSponsors = [{
-  name: 'くろぐろ',
-  image: 'https://github.com/k-kuroguro.png',
-  url: 'https://twitter.com/k_kuroguro',
-}]
+import {
+  sponsors,
+  personalSponsors,
+  personalBronzeSponsors,
+  personalSilverSponsors,
+} from '@/sponsors'
 </script>
---
+++ sponsors.ts
@@ -0,0 +1,35 @@
+export type Sponsor = { name: string, image: string, url: string }
+export type Sponsors = Sporsor[]
+
+export const sponsors: Sponsors = [{
+  name: 'CubeSoft, Inc.',
+  image: 'https://raw.githubusercontent.com/cube-soft/Cube.Core/master/Icon.svg',
+  url: 'https://www.cube-soft.jp/',
+}]
+
+export const personalSilverSponsors: Sponsors = [{
+  name: 'takesako',
+  image: 'https://github.com/takesako.png',
+  url: 'https://github.com/takesako',
+}, {
+  name: 'orisano',
+  image: 'https://github.com/orisano.png',
+  url: 'https://github.com/orisano',
+}]
+
+export const personalBronzeSponsors: Sponsors = [{
+  name: 'Krouton',
+  image: 'https://github.com/Krout0n.png',
+  url: 'https://github.com/Krout0n',
+}, {
+  name: 'Kazuki Chigita',
+  image: 'https://pbs.twimg.com/profile_images/1480844416462225409/aiBGMxvt.jpg',
+  url: 'https://kazuki.chigita.net/profile',
+}]
+
+export const personalSponsors: Sponsors = [{
+  name: 'くろぐろ',
+  image: 'https://github.com/k-kuroguro.png',
+  url: 'https://twitter.com/k_kuroguro',
+}]
+
