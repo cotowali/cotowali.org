0ee6b4dabf4e5c2053a74646786f57a1f7ba762e
zakuro <z@kuro.red>
2023-03-04 09:19:45 +0900

    format sponsors.ts

--- sponsors.ts
+++ sponsors.ts
@@ -1,42 +1,53 @@
 export type Sponsor = { name: string, image: string, url: string }
 export type Sponsors = Sporsor[]
 
-export const sponsors: Sponsors = [{
-  name: 'CubeSoft, Inc.',
-  image: 'https://raw.githubusercontent.com/cube-soft/Cube.Core/master/Icon.svg',
-  url: 'https://www.cube-soft.jp/',
-}]
+export const sponsors: Sponsors = [
+  {
+    name: 'CubeSoft, Inc.',
+    image: 'https://raw.githubusercontent.com/cube-soft/Cube.Core/master/Icon.svg',
+    url: 'https://www.cube-soft.jp/',
+  },
+]
 
-export const personalSilverSponsors: Sponsors = [{
-  name: 'takesako',
-  image: 'https://github.com/takesako.png',
-  url: 'https://github.com/takesako',
-}, {
-  name: 'orisano',
-  image: 'https://github.com/orisano.png',
-  url: 'https://github.com/orisano',
-}]
+export const personalSilverSponsors: Sponsors = [
+  {
+    name: 'takesako',
+    image: 'https://github.com/takesako.png',
+    url: 'https://github.com/takesako',
+  }, {
+    name: 'orisano',
+    image: 'https://github.com/orisano.png',
+    url: 'https://github.com/orisano',
+  },
+]
 
-export const personalBronzeSponsors: Sponsors = [{
-  name: 'Krouton',
-  image: 'https://github.com/Krout0n.png',
-  url: 'https://github.com/Krout0n',
-}, {
-  name: 'Kazuki Chigita',
-  image: 'https://kazuki.chigita.net/chigichan24_2022.png',
-  url: 'https://kazuki.chigita.net/profile',
-}]
+export const personalBronzeSponsors: Sponsors = [
+  {
+    name: 'Krouton',
+    image: 'https://github.com/Krout0n.png',
+    url: 'https://github.com/Krout0n',
+  },
+  {
+    name: 'Kazuki Chigita',
+    image: 'https://kazuki.chigita.net/chigichan24_2022.png',
+    url: 'https://kazuki.chigita.net/profile',
+  },
+]
 
-export const personalSponsors: Sponsors = [{
-  name: 'くろぐろ',
-  image: 'https://github.com/k-kuroguro.png',
-  url: 'https://twitter.com/k_kuroguro',
-}, {
-  name: 'かずひら',
-  image: 'https://unavatar.io/twitter/kazuhi_ra',
-  url: 'https://twitter.com/kazuhi_ra',
-}, {
-  name: 'ヒツジのしっぽ',
-  image: 'https://github.com/hituzi-no-sippo.png',
-  url: 'https://github.com/hituzi-no-sippo',
-}]
+export const personalSponsors: Sponsors = [
+  {
+    name: 'くろぐろ',
+    image: 'https://github.com/k-kuroguro.png',
+    url: 'https://twitter.com/k_kuroguro',
+  },
+  {
+    name: 'かずひら',
+    image: 'https://unavatar.io/twitter/kazuhi_ra',
+    url: 'https://twitter.com/kazuhi_ra',
+  },
+  {
+    name: 'ヒツジのしっぽ',
+    image: 'https://github.com/hituzi-no-sippo.png',
+    url: 'https://github.com/hituzi-no-sippo',
+  },
+]
